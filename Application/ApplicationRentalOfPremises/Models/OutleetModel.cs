d5df036c5a9a05b7b5445798caa779f0dd9c373a
Dima <73338242+Dima3108@users.noreply.github.com>
2024-09-15 14:45:24 +0300

    Add class entity

---
+++ Application/ApplicationRentalOfPremises/Models/OutleetModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationRentalOfPremises.Models
+{
+    /// <summary>
+    /// Торговая точка
+    /// </summary>
+    public class OutletModel
+    {
+        public int ID {  get; set; }
+        /// <summary>
+        /// Этаж
+        /// </summary>
+        public int Storey {  get; set; }  
+        /// <summary>
+        /// Площадь помещения
+        /// </summary>
+        public int Area {  get; set; }
+        /// <summary>
+        /// Наличие кондиционера
+        /// </summary>
+        public short PresenceOfAirConditioning {  get; set; }  
+        /// <summary>
+        /// Стоимость аренды в день
+        /// </summary>
+        public int RentalCostPerDay {  get; set; }  
+    }
+}
