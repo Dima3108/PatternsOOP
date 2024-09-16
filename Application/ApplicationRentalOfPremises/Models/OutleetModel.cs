9b174627fe413b9c77bbceeb194f9980f652b3c4
Dima <73338242+Dima3108@users.noreply.github.com>
2024-09-16 18:11:34 +0300

    Add Setter
    Добавил Setter

--- Application/ApplicationRentalOfPremises/Models/OutleetModel.cs
+++ Application/ApplicationRentalOfPremises/Models/OutleetModel.cs
@@ -19,23 +19,59 @@ namespace ApplicationRentalOfPremises.Models
         /// </summary>
         [Required]
         public int Storey {  get; set; }
+        public int SetStorey(int St)
+        {
+            this.Storey = St;
+            return 0;
+        }
         /// <summary>
         /// Площадь помещения
         /// </summary>
         [Required]
-        [Range(0,int.MaxValue)]
+        //[Range(0,int.MaxValue)]
         public int Area {  get; set; }
+        public int SetArea(int area)
+        {
+            if (Area < 0)
+                return -1;
+            this.Area = area;
+            return 0;
+        }
         /// <summary>
         /// Наличие кондиционера
         /// </summary>
         [Required]
-        [Range(0, int.MaxValue)]
+        [Range(0, 1)]
         public short PresenceOfAirConditioning {  get; set; }
+        public int SetPresenceOfAirConditioning(short POAC)
+        {
+            if (POAC > 1 || POAC < 0)
+                return -1;
+            this.PresenceOfAirConditioning = POAC;
+            return 0;
+        }
         /// <summary>
         /// Стоимость аренды в день
         /// </summary>
         [Required]
-        [Range(0, int.MaxValue)]
+        //[Range(0, int.MaxValue)]
         public int RentalCostPerDay {  get; set; }  
+        public int SetRentalCostPerDay(int RCPD)
+        {
+            if (RCPD < 0)
+                return -1;
+            this.RentalCostPerDay = RCPD;
+            return 0;
+        }
+        public OutletModel(int ID,int Storege,int Area,short PresenceOfAirConditining,int RentalCostPerDay)
+        {
+            int p = 1;
+          int  error=  SetStorey(Storege);
+           error+=-1*((int)(Math.Pow(2,p++)))* SetArea(Area);
+            error += -1 * ((int)(Math.Pow(2, p++))) * SetPresenceOfAirConditioning(PresenceOfAirConditining);
+            error += -1 * ((int)(Math.Pow(2, p++))) * SetRentalCostPerDay(RentalCostPerDay);
+
+        }
+       
     }
 }
