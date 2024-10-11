6f74a69119d307d2aa80c27aaa0db2f1bbaaceff
Dima <73338242+Dima3108@users.noreply.github.com>
2024-10-11 21:08:52 +0300

    UPDATE DIAGRAM
    Исправил диаграмму , добавил стратегию для хранилищ , а также паттерн Шаблон для базы данных (Инициализация таблиц )

--- Application/ApplicationRentalOfPremises/obj/Debug/net5.0/ApplicationRentalOfPremises.AssemblyInfo.cs
+++ Application/ApplicationRentalOfPremises/obj/Debug/net5.0/ApplicationRentalOfPremises.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApplicationRentalOfPremises")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee93cbe26e7880e6b3ef00ecce0bb05629570183")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fa64b01776aade14d7fafbccf3c7f2127aba0cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApplicationRentalOfPremises")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApplicationRentalOfPremises")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
