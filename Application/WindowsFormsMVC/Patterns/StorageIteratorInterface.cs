711ea4c328840f00b13c480d154ff5e3cd5f515c
Dima <73338242+Dima3108@users.noreply.github.com>
2024-10-26 22:53:58 +0300

    Add class diagram
    Добавил диаграмму класов

--- Application/WindowsFormsMVC/Patterns/StorageIteratorInterface.cs
+++ Application/WindowsFormsMVC/Patterns/StorageIteratorInterface.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using ApplicationRentalOfPremises.Models;
 namespace WindowsFormsMVC.Patterns
 {
-    public interface StorageIteratorInterface : IEnumerator<OutleetSmallModel>
+    public interface StorageIteratorInterface //: IEnumerator<OutleetSmallModel>
     {
         OutleetSmallModel Next();
         bool IsNext();
